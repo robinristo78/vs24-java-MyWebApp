3890df5c70bca7c6a23a259be5b94b026bfa1e14
robinristo78 <robin-risto.kivit@voco.ee>
2025-11-13 10:50:33 +0200

    Project refactor from Gradle to Maven

---
+++ src/main/java/ch/makery/vs24javamywebapp/Vs24JavaMyWebAppApplication.java
@@ -0,0 +1,13 @@
+package ch.makery.vs24javamywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vs24JavaMyWebAppApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(Vs24JavaMyWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/makery/vs24javamywebapp/Vs24JavaMyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.makery.vs24javamywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vs24JavaMyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
