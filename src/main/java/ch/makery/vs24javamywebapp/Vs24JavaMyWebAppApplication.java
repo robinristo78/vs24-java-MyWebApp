499dc3eb9b4bd1fd21a2571476aa7b905d714fbb
robinristo78 <robin-risto.kivit@voco.ee>
2025-11-25 15:29:53 +0200

    Final Tomcat Build

--- src/main/java/ch/makery/vs24javamywebapp/Vs24JavaMyWebAppApplication.java
+++ src/main/java/ch/makery/vs24javamywebapp/Vs24JavaMyWebAppApplication.java
@@ -2,12 +2,18 @@ package ch.makery.vs24javamywebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Vs24JavaMyWebAppApplication {
+public class Vs24JavaMyWebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
         SpringApplication.run(Vs24JavaMyWebAppApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Vs24JavaMyWebAppApplication.class);
+    }
 }
